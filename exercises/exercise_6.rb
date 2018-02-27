7b15aca488131c60b866d9c647ed62f7a9cdf49e
michaelc <michael.38@outlook.com>
2018-02-27 12:53:43 -0500

    completed exercise_6: add relational data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,13 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create!(first_name: "Michael", last_name: "Scott", hourly_rate: 80)
+@store1.employees.create!(first_name: "Jim", last_name: "Halpert", hourly_rate: 60)
+@store1.employees.create!(first_name: "Dwight", last_name: "Schrute", hourly_rate: 50)
+
+@store2.employees.create!(first_name: "Oscar", last_name: "Martinez", hourly_rate: 45)
+@store2.employees.create!(first_name: "Kevin", last_name: "Malone", hourly_rate: 40)
+@store2.employees.create!(first_name: "Angela", last_name: "Martin", hourly_rate: 55)
+
+p @store1.employees
+p @store2.employees
