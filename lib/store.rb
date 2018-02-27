f69194c1a523c94e57f6addd69ecad25b0949bef
michaelc <michael.38@outlook.com>
2018-02-27 13:08:37 -0500

    revised validation and error

--- lib/store.rb
+++ lib/store.rb
@@ -7,11 +7,14 @@ class Store < ActiveRecord::Base
         greater_than_or_equal_to: 0
     }
     validates :mens_apparel, inclusion: { in: [true, false] }
+    validates :womens_apparel, inclusion: { in: [true, false] }
 
     validate :carry_mens_or_womens
 
+    private
     def carry_mens_or_womens
         if mens_apparel == false && womens_apparel == false
+            errors.add(:mens_apparel, "store must carry either men's or women's apparel")
             errors.add(:womens_apparel, "store must carry either men's or women's apparel")
         end
     end
