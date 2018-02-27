825149fb3b01366f2c91b5f5b1c7f1e804bac575
michaelc <michael.38@outlook.com>
2018-02-27 08:58:31 -0500

    completed exercise_1: add to database

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,31 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
+
+Store.create(
+  name: "Burnaby",
+  annual_revenue: 300000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+Store.create(
+  name: "Richmond",
+  annual_revenue: 1260000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+Store.create(
+  name: "Gastown",
+  annual_revenue: 190000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+
+
+puts Stores.count
+
+
 # Your code goes below here ...
