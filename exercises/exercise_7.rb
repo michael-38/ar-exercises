f42804fcd1e345dcf62df60c098d325f7d7bae5f
michaelc <michael.38@outlook.com>
2018-02-27 12:54:25 -0500

    completed exercise_7: validations/errors

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,33 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+
+puts "---Create a store---"
+
+print "Enter store name: "
+@store_name = gets.chomp
+
+print "Enter store's annual revenue: "
+@annual_revenue = gets.chomp
+
+print "Does the store carry men's apparel (true/false): "
+@mens_apparel = gets.chomp
+
+print "Does the store carry men's apparel (true/false): "
+@womens_apparel = gets.chomp
+
+
+@new_store = Store.create(
+  name: @store_name,
+  annual_revenue: @annual_revenue,
+  mens_apparel: @mens_apparel,
+  womens_apparel: @womens_apparel
+)
+
+# p Store.order('id ASC')
+
+# puts @new_store.errors.full_messages.to_sentence
+
+@new_store.errors.full_messages.each do |message|
+  puts message
+end 
